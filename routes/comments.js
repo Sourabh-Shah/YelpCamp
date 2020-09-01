3e4e4f0771f2e9df63bcd09683dd75cd24b2835c
unknown <ssourabhshah@gmail.com>
2020-09-01 11:09:23 +0530

    Added authorization to comments

--- routes/comments.js
+++ routes/comments.js
@@ -44,6 +44,62 @@ router.post("/campgrounds/:id/comments",isLoggedIn,  function(req, res){
 		}
 	});
 });
+// comment edit route
+router.get("/campgrounds/:id/comments/:comment_id/edit", checkCommentOwnership ,function(req, res){
+	comment.findById(req.params.comment_id, function(err, foundComment){
+		if (err){
+			res.redirect("back");
+		}
+		else {
+			res.render("./comments/edit",{campground_id : req.params.id , comment : foundComment});
+		}
+	})	;
+});
+
+// Comment update
+router.put("/campgrounds/:id/comments/:comment_id", checkCommentOwnership ,function(req, res){
+
+	comment.findByIdAndUpdate(req.params.comment_id,req.body.comment, function(err, updateComment){
+		if (err){
+			res.redirect("back");
+		}
+		else{
+			res.redirect("/campgrounds/"+ req.params.id);
+		} 
+	} );
+});
+
+// Delete comment route
+router.delete("/campgrounds/:id/comments/:comment_id", checkCommentOwnership ,function(req, res){
+	comment.findByIdAndRemove(req.params.comment_id,function(err){
+		if (err){
+			res.redirect("back");
+		}
+		else{
+			res.redirect("/campgrounds/"+req.params.id);
+		}
+	});
+});
+// middleware
+function checkCommentOwnership(req, res, next){
+	if (req.isAuthenticated()){
+		comment.findById(req.params.comment_id, function(err, foundComment){
+			if (err){
+				res.redirect("back");
+			}else {
+				if (foundComment.author.id.equals(req.user._id)){
+					next()
+				}
+				else{
+					res.redirect("back");
+				}
+			}
+		});
+	}
+	else {
+		res.redirect("back");
+	}
+}
 function isLoggedIn(req, res, next){
 	if (req.isAuthenticated()){
 		return next();
