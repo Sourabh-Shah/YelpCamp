f14b33d625232f78d7fbe54cfa479017240f4d90
unknown <ssourabhshah@gmail.com>
2020-09-03 22:26:20 +0530

    refactored the port

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use(indexRoutes);
 app.use(campgroundRoutes);
 app.use(commentRoutes);
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,,function(){
 	console.log("YelpCamp server has started...");
 })
